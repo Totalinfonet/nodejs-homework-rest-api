886d063eb05d6382025574f9f0201fbcae87c670
Totalinfonet <totalinfonet@gmail.com>
2023-06-19 14:44:38 +0300

    Register Controller Added

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,17 @@
 const express = require("express");
 
+const ctrl = require("../../controllers/auth");
+
+const { validatePostBody } = require("../../middlewares");
+
+const schemas = require("../../schemas/users");
+
 const router = express.Router();
 
-router.post("/register");
+router.post(
+  "/register",
+  validatePostBody(schemas.registerSchema),
+  ctrl.register
+);
 
 module.exports = router;
--- schemas/contacts.js
+++ schemas/contacts.js
@@ -10,20 +10,7 @@ const contactFavoriteUpdateSchema = Joi.object({
   favorite: Joi.boolean().required(),
 });
 
-const registerSchema = Joi.object({
-  name: Joi.string().required(),
-  email: Joi.string().email().required(),
-  password: Joi.string().min(6).required(),
-});
-
-const loginSchema = Joi.object({
-  email: Joi.string().email().required(),
-  password: Joi.string().min(6).required(),
-});
-
 module.exports = {
   contactAddSchema,
   contactFavoriteUpdateSchema,
-  registerSchema,
-  loginSchema,
 };
