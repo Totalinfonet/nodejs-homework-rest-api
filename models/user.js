cf33ff5f990eef134a678308db0954cca1ba7a75
Totalinfonet <totalinfonet@gmail.com>
2023-06-19 15:39:41 +0300

    Unique Check Added

--- models/user.js
+++ models/user.js
@@ -5,7 +5,7 @@ const { handleMongooseError } = require("../helpers");
 const userSchema = new Schema(
   {
     name: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
     password: { type: String, minlength: 6, required: true },
   },
   { versionKey: false, timestamps: true }
